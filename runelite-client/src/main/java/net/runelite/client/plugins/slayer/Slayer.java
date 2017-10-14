7e75a5fbed5689798ec726b5018499697143bc62
Tyler Hardy <tylerthardy@gmail.com>
2017-10-13 21:13:34 -0500

    Make system to track kill amount
    Create overlay
    
    Save task info on logout
    
    Monster name uppercase
    
    Remove unused imports
    
    Fix for id'd monsters counting on other tasks
    
    Checkstyle
    
    Restructure to no longer use npcId
    
    Rename 'monster' to 'taskName'

---
+++ runelite-client/src/main/java/net/runelite/client/plugins/slayer/Slayer.java
@@ -0,0 +1,134 @@
+/*
+ * Copyright (c) 2017, Tyler <https://github.com/tylerthardy>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package net.runelite.client.plugins.slayer;
+
+import com.google.common.eventbus.Subscribe;
+import net.runelite.api.Actor;
+import net.runelite.api.NPC;
+import net.runelite.client.RuneLite;
+import net.runelite.client.events.ActorDeath;
+import net.runelite.client.events.ChatMessage;
+import net.runelite.client.plugins.Plugin;
+import net.runelite.client.ui.overlay.Overlay;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Slayer extends Plugin
+{
+	private final SlayerConfig config = RuneLite.getRunelite().getConfigManager().getConfig(SlayerConfig.class);
+	private final SlayerOverlay overlay = new SlayerOverlay(this);
+
+	private final Pattern taskMsgPattern = Pattern.compile("You're assigned to kill (.*?)s?; only (\\d*) more to go\\.");
+
+	private String taskName;
+	private int amount;
+
+	@Override
+	protected void startUp() throws Exception
+	{
+		if (config.amount() != -1 && !config.taskName().equals(""))
+		{
+			setTask(config.taskName(), config.amount());
+		}
+	}
+
+	@Override
+	protected void shutDown() throws Exception
+	{
+	}
+
+	private void save()
+	{
+		config.amount(this.amount);
+		config.taskName(this.taskName);
+	}
+
+	@Override
+	public Overlay getOverlay()
+	{
+		return overlay;
+	}
+
+	@Subscribe
+	public void onChatMessage(ChatMessage event)
+	{
+		Matcher m = taskMsgPattern.matcher(event.getMessage());
+
+		if (!m.find())
+			return;
+
+		String taskName = m.group(1);
+		int amount = Integer.parseInt(m.group(2));
+
+		setTask(taskName, amount);
+	}
+
+	@Subscribe
+	public void onActorDeath(ActorDeath death)
+	{
+		Actor actor = death.getActor();
+		if (actor instanceof NPC)
+		{
+			NPC npc = (NPC)actor;
+			String npcName = npc.getName().toLowerCase();
+
+			System.out.println("killed:" + npcName);
+			System.out.println(String.format("%s/%s", taskName, npcName));
+
+			//TODO: you need to check plurals
+			if (npcName.equals(taskName) || Task.isNpcFromTask(npcName, taskName))
+			{
+				killedOne();
+			}
+			System.out.println(taskName + ":" + amount);
+		}
+	}
+
+	private void killedOne()
+	{
+		amount--;
+		save(); //Inefficient, but RL is not running plugins' shutDown method. Move there once fixed.
+	}
+
+	private void setTask(String taskName, int amount)
+	{
+		this.taskName = taskName.toLowerCase();
+		this.amount = amount;
+		save();
+
+		System.out.println("task set:" + this.taskName + ":" + this.amount);
+	}
+
+	public String getTaskName()
+	{
+		return taskName;
+	}
+
+	public int getAmount()
+	{
+		return amount;
+	}
+}
---
+++ runelite-client/src/main/java/net/runelite/client/plugins/slayer/Task.java
@@ -0,0 +1,166 @@
+/*
+ * Copyright (c) 2017, Tyler <https://github.com/tylerthardy>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package net.runelite.client.plugins.slayer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+
+enum Task
+{
+	//<editor-fold desc="Enums">
+	ABERRANT_SPECTRE("aberrant spectre", new HashSet<>(Arrays.asList("deviant spectre"))),
+	ABYSSAL_DEMON("abyssal demon", new HashSet<>(Arrays.asList("abyssal sire"))),
+	ANKOU("ankou", null),
+	BANSHEE("banshee", null),
+	BASILISK("basilisk", null),
+	BAT("bat", new HashSet<>(Arrays.asList("giant bat"))),
+	BEAR("bear", new HashSet<>(Arrays.asList("black bear","grizzly bear","callisto"))),
+	BIRD("bird", new HashSet<>(Arrays.asList("bird","chicken","mounted terrorbird gnome","rooster","terrorbird","vulture","undead chicken"))),
+	BLACK_DEMON("black demon", new HashSet<>(Arrays.asList("demonic gorilla","balfrug kreeyath","skotizo"))),
+	BLACK_DRAGON("black dragon", new HashSet<>(Arrays.asList("baby black dragon","king black dragon","brutal black dragon"))),
+	BLOODVELD("bloodveld", new HashSet<>(Arrays.asList("mutated bloodveld"))),
+	BLUE_DRAGON("blue dragon", new HashSet<>(Arrays.asList("baby blue dragon","brutal blue dragon"))),
+	BRINE_RAT("brine rat", null),
+	BRONZE_DRAGON("bronze dragon", null),
+	CATABLEPON("catablepon", null),
+	CAVE_BUG("cave bug", null),
+	CAVE_CRAWLER("cave crawler", null),
+	CAVE_HORROR("cave horror", null),
+	CAVE_SLIME("cave slime", null),
+	COCKATRICE("cockatrice", null),
+	COW("cow", null),
+	CRAWLING_HAND("crawling hand", null),
+	CROCODILE("crocodile", null),
+	DAGANNOTH("dagannoth", new HashSet<>(Arrays.asList("dagannoth fledgeling","dagannoth spawn","dagannoth prime","dagannoth rex","dagannoth supreme"))),
+	DARK_BEAST("dark beast", null),
+	DESERT_LIZARD("desert lizard", new HashSet<>(Arrays.asList("lizard","small lizard"))),
+	DOG("dog", new HashSet<>(Arrays.asList("guard dog","wild dog","jackal"))),
+	DUST_DEVIL("dust devil", null),
+	DWARF("dwarf", new HashSet<>(Arrays.asList("black guard","chaos dwarf"))),
+	EARTH_WARRIOR("earth warrior", null),
+	ELF("elf", new HashSet<>(Arrays.asList("mourner (lvl 108 variant only)"))),
+	FEVER_SPIDER("fever spider", null),
+	FIRE_GIANT("fire giant", null),
+	FLESH_CRAWLER("flesh crawler", null),
+	GARGOYLE("gargoyle", null),
+	GHOST("ghost", new HashSet<>(Arrays.asList("tortured soul"))),
+	GHOUL("ghoul", new HashSet<>(Arrays.asList("ghoul champion"))),
+	GOBLIN("goblin", new HashSet<>(Arrays.asList("goblin champion","sergeant strongstack","sergeant grimspike","sergeant steelwill"))),
+	GORAK("gorak", null),
+	GREATER_DEMON("greater demon", new HashSet<>(Arrays.asList("kril tsutsaroth","tstanon karlak","skotizo"))),
+	GREEN_DRAGON("green dragon", new HashSet<>(Arrays.asList("baby green dragon","brutal green dragon"))),
+	HARPIE_BUG_SWARM("harpie bug swarm", null),
+	HELLHOUND("hellhound", new HashSet<>(Arrays.asList("cerberu"))),
+	HILL_GIANT("hill giant", new HashSet<>(Arrays.asList("giant champion"))),
+	HOBGOBLIN("hobgoblin", new HashSet<>(Arrays.asList("hobgoblin champion"))),
+	ICEFIEND("icefiend", null),
+	ICE_GIANT("ice giant", null),
+	ICE_WARRIOR("ice warrior", null),
+	INFERNAL_MAGE("infernal mage", null),
+	IRON_DRAGON("iron dragon", null),
+	JELLY("jelly", new HashSet<>(Arrays.asList("warped jelly"))),
+	JUNGLE_HORROR("jungle horror", null),
+	KALPHITE("kalphite", new HashSet<>(Arrays.asList("kalphite queen","kalphite worker","kalphite soldier","kalphite guardian"))),
+	KILLERWATT("killerwatt", null),
+	KURASK("kurask", null),
+	LESSER_DEMON("lesser demon", null),
+	MITHRIL_DRAGON("mithril dragon", null),
+	MINOTAUR("minotaur", null),
+	MOGRE("mogre", null),
+	MOLANISK("molanisk", null),
+	MONKEY("monkey", new HashSet<>(Arrays.asList("monkey guard","monkey archer","zombie monkey and more (like padulah)"))),
+	MOSS_GIANT("moss giant", null),
+	NECHRYAEL("nechryael", null),
+	OGRE("ogre", new HashSet<>(Arrays.asList("ogre chieftain","enclave guard"))),
+	OTHERWORLDLY_BEING("otherworldly being", null),
+	PYREFIEND("pyrefiend", null),
+	RED_DRAGON("red dragon", new HashSet<>(Arrays.asList("baby red dragon","brutal red dragon"))),
+	ROCKSLUG("rockslug", null),
+	SCABARITE("scabarite", new HashSet<>(Arrays.asList("locust rider","scarab mage","scarab swarm"))),
+	SCORPION("scorpion", new HashSet<>(Arrays.asList("king scorpion","poison scorpion","pit scorpion","scorpia"))),
+	SEA_SNAKE("sea snake", new HashSet<>(Arrays.asList("sea snake hatchling","sea snake young","giant sea snake"))),
+	SHADE("shade", new HashSet<>(Arrays.asList("loar","phryn","riyl","asyn and fiyr shade"))),
+	SHADOW_WARRIOR("shadow warrior", null),
+	SKELETAL_WYVERN("skeletal wyvern", null),
+	SKELETON("skeleton", new HashSet<>(Arrays.asList("skeleton champion","vetion"))),
+	SMOKE_DEVIL("smoke devil", new HashSet<>(Arrays.asList("thermonuclear smoke devil","nuclear smoke devil"))),
+	SPIDER("spider", new HashSet<>(Arrays.asList("giant spider","shadow spider","jungle spider","deadly red spider","poison spider","blessed spider","kalrag","crypt spider"))),
+	STEEL_DRAGON("steel dragon", null),
+	SUQAH("suqah", null),
+	TERROR_DOG("terror dog", null),
+	TROLL("troll", new HashSet<>(Arrays.asList("mountain troll","ice troll grunt","runt","male and female","ice troll","river troll"))),
+	TUROTH("turoth", null),
+	VAMPIRE("vampire", null),
+	WALL_BEAST("wall beast", null),
+	WATERFIEND("waterfiend", null),
+	WEREWOLF("werewolf", null),
+	WOLF("wolf", new HashSet<>(Arrays.asList("big wolf","dire wolf","jungle wolf","desert wolf","ice wolf"))),
+	ZOMBIE("zombie", new HashSet<>(Arrays.asList("any zombie-class except zogres and zombie monkey"))),
+	ZYGOMITE("zygomite", new HashSet<>(Arrays.asList("ancient zygomite")));
+	//</editor-fold>
+
+	private static final Logger logger = LoggerFactory.getLogger(Task.class);
+
+	private static final Map<String, Task> tasks = new HashMap<>();
+
+	private final String name;
+	private final HashSet<String> alternatives;
+
+	static
+	{
+		for (Task task : values())
+		{
+			tasks.put(task.getName(), task);
+		}
+	}
+
+	Task(String name, HashSet<String> alternatives)
+	{
+		this.name = name;
+		this.alternatives = alternatives;
+	}
+
+	public String getName()
+	{
+		return this.name;
+	}
+
+	public static boolean isNpcFromTask(String npcName, String taskName)
+	{
+		System.out.println(npcName);
+		System.out.println(taskName);
+
+		Task task = tasks.get(taskName);
+		if (task == null)
+			return false;
+
+		return task.alternatives.contains(npcName);
+	}
+}
