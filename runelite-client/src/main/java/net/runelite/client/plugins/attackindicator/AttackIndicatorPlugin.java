361da907cda9fec2e39ec2f0c32ae0b74e1e32fb
Tomas Slusny <slusnucky@gmail.com>
2018-02-04 19:59:31 +0100

    Remove "plugin" from each plugin name
    Having "plugin" in plugin names is completely redundant and should not
    be there.
    
    Signed-off-by: Tomas Slusny <slusnucky@gmail.com>

--- runelite-client/src/main/java/net/runelite/client/plugins/attackindicator/AttackIndicatorPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/attackindicator/AttackIndicatorPlugin.java
@@ -54,7 +54,7 @@ import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
 
 @PluginDescriptor(
-	name = "Attack indicator plugin"
+	name = "Attack indicator"
 )
 @Slf4j
 public class AttackIndicatorPlugin extends Plugin
