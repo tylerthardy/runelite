6594c3346ebe72addd8f5b930e8828b68dc0d237
Seth <sethtroll3@gmail.com>
2018-02-17 16:08:38 -0600

    Remove @Nullable Client annotations
    A check was added for plugins not to be loaded when loading vanilla thus the client is always non-null. Plugins loaded with vanilla don't use client.

--- runelite-client/src/main/java/net/runelite/client/plugins/mousehighlight/MouseHighlightOverlay.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/mousehighlight/MouseHighlightOverlay.java
@@ -28,7 +28,6 @@ import com.google.common.base.Strings;
 import java.awt.Dimension;
 import java.awt.Graphics2D;
 import java.awt.Point;
-import javax.annotation.Nullable;
 import javax.inject.Inject;
 import net.runelite.api.Client;
 import net.runelite.api.MenuEntry;
@@ -43,7 +42,7 @@ class MouseHighlightOverlay extends Overlay
 	private final Client client;
 
 	@Inject
-	MouseHighlightOverlay(@Nullable Client client, TooltipManager tooltipManager)
+	MouseHighlightOverlay(Client client, TooltipManager tooltipManager)
 	{
 		setPosition(OverlayPosition.DYNAMIC);
 		this.client = client;
